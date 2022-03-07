d8c1bf2d9d07608db485d0a9b6fb1b2cfb65093f
“sanchez-pkm” <masanchez@bitso.com>
2022-03-07 12:05:23 -0600

    update publisher

--- src/main/java/com/aeroncookbook/ipc/BasicPublisher.java
+++ src/main/java/com/aeroncookbook/ipc/BasicPublisher.java
@@ -19,6 +19,7 @@ import io.aeron.Aeron;
 import io.aeron.Publication;
 import io.aeron.driver.MediaDriver;
 import org.agrona.BufferUtil;
+import org.agrona.CloseHelper;
 import org.agrona.concurrent.UnsafeBuffer;
 
 import java.util.concurrent.TimeUnit;
@@ -35,81 +36,72 @@ import java.util.concurrent.TimeUnit;
  * -Daeron.sample.channel=aeron:udp?endpoint=localhost:5555 -Daeron.sample.streamId=20
  */
 public class BasicPublisher {
-    private final int streamId;//8;
-    private final String channel; //"aeron:udp?endpoint=localhost:8081";
-    private final String content;
-    private static final long LINGER_TIMEOUT_MS = 5000;
 
-    final int n_messages;
+    private final int streamId;
+    private final String channel;
 
-    public BasicPublisher(String channel, int streamId, String content, int n_messages) {
-        this.n_messages = n_messages;
-        this.channel = channel;
+    public BasicPublisher(String channel, int streamId) {
         this.streamId = streamId;
-        this.content = content;
+        this.channel = channel;
     }
 
-    public void send() {
+    public static void main(String[] args) throws InterruptedException {
+        //new BasicPublisher("aeron:udp?endpoint=localhost:8081", 8).send("teste");
+    }
 
-        final Aeron.Context ctx = new Aeron.Context();
+    public long send(String message) throws InterruptedException {
+        System.out.println("Publishing to " + channel + " on stream id " + streamId);
+
+        // If configured to do so, create an embedded media driver within this application rather
+        // than relying on an external one.
         final MediaDriver driver = MediaDriver.launchEmbedded();
 
-        ctx.aeronDirectoryName(driver.aeronDirectoryName()); ///dev/shm
+        final Aeron.Context ctx = new Aeron.Context();
 
-        // Connect a new Aeron instance to the media driver and create a publication on
-        // the given channel and stream ID.
-        // The Aeron and Publication classes implement "AutoCloseable" and will automatically
-        // clean up resources when this try block is finished
+        ctx.aeronDirectoryName(driver.aeronDirectoryName());
+
+        long result;
 
         try (
                 Aeron aeron = Aeron.connect(ctx);
                 Publication publication = aeron.addPublication(channel, streamId)
         ) {
-
             final UnsafeBuffer buffer = new UnsafeBuffer(BufferUtil.allocateDirectAligned(256, 64));
 
-            for (long i = 0; i < n_messages; i++) {
-                System.out.print("Offering " + i + "/" + n_messages + " - ");
+            System.out.println("Sending - " + message);
 
-                final int length = buffer.putStringWithoutLengthAscii(0, content);
-                final long result = publication.offer(buffer, 0, length);
+            final int length = buffer.putStringWithoutLengthAscii(0, message);
 
-                if (result > 0) {
-                    System.out.println("Sending ... :) !");
-                } else if (result == Publication.BACK_PRESSURED) {
-                    System.out.println("Offer failed due to back pressure");
-                } else if (result == Publication.NOT_CONNECTED) {
-                    System.out.println("Offer failed because publisher is not connected to a subscriber");
-                } else if (result == Publication.ADMIN_ACTION) {
-                    System.out.println("Offer failed because of an administration action in the system");
-                } else if (result == Publication.CLOSED) {
-                    System.out.println("Offer failed because publication is closed");
-                    break;
-                } else if (result == Publication.MAX_POSITION_EXCEEDED) {
-                    System.out.println("Offer failed due to publication reaching its max position");
-                    break;
-                } else {
-                    System.out.println("Offer failed due to unknown reason: " + result);
-                }
+            Thread.sleep(TimeUnit.SECONDS.toMillis(1));
 
-                if (!publication.isConnected()) {
-                    System.out.println("No active subscribers detected");
-                }
+            result = publication.offer(buffer, 0, length);
 
-                Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+            if (result > 0) {
+                System.out.println("sent");
+            } else if (result == Publication.BACK_PRESSURED) {
+                System.out.println("Offer failed due to back pressure");
+            } else if (result == Publication.NOT_CONNECTED) {
+                System.out.println("Offer failed because publisher is not connected to a subscriber");
+            } else if (result == Publication.ADMIN_ACTION) {
+                System.out.println("Offer failed because of an administration action in the system");
+            } else if (result == Publication.CLOSED) {
+                System.out.println("Offer failed because publication is closed");
+            } else if (result == Publication.MAX_POSITION_EXCEEDED) {
+                System.out.println("Offer failed due to publication reaching its max position");
+            } else {
+                System.out.println("Offer failed due to unknown reason: " + result);
             }
 
-            System.out.println("Done sending...");
-
-            if (LINGER_TIMEOUT_MS > 0) {
-                System.out.println("Lingering for " + LINGER_TIMEOUT_MS + " milliseconds...");
-                Thread.sleep(LINGER_TIMEOUT_MS);
+            if (!publication.isConnected()) {
+                System.out.println("No active subscribers detected");
             }
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            ctx.close();
+            System.out.println("Done sending.");
+
         }
+
+        CloseHelper.close(driver);
+
+        return result;
     }
 }
